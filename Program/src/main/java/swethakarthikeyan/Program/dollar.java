fe837b76c5c45289255f32bc2cb0b96ee3bb6974
swetha311999 <swe99swetha@gmail.com>
2020-10-20 13:26:45 +0530

    basics

---
+++ Program/src/main/java/swethakarthikeyan/Program/dollar.java
@@ -0,0 +1,13 @@
+package swethakarthikeyan.Program;
+import java.util.Scanner;
+public class dollar {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int dollar=scan.nextInt();
+		double rupees=dollar*73.14;
+		System.out.println("the amount is:"+rupees);
+		
+		
+	}
+
+}
